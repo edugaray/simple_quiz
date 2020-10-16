9b4e0db7e75174f769dc156e45ccf8dcfde54282
Edu Garay <edugaray@edus-mbp.mynet>
2020-10-16 13:58:27 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+let counter = parseInt(document.querySelector('.counter strong').innerHTML);
+let score = 0;
+const updatecounters = function () {
+  if (this.classList.contains('is-correct')) {
+    score += 1;
+  }
+  // else {
+  //   console.log(this.querySelector('is-correct'));
+  // }
+  counter += 1;
+  let element = this.closest('.trivia-question');
+  this.closest('.quiz-answers-wrapper').classList.add('clicked');
+  element.classList.add('hiding');
+
+  setTimeout(function () {
+    element.classList.add('hidden');
+    document.getElementById(counter).classList.remove('hidden');
+    document.querySelector('.counter p strong').innerHTML = counter, false;  }, 2000);
+    if (counter === 16) {
+      document.querySelector('.quiz-container').classList.add('hidden');
+      document.querySelector('.quiz-score').classList.remove('hidden');
+      document.querySelector('.quiz-score').innerHTML = ('<div class="finalscore"><h2>You got '+score+' correct out of 15</p></div>')
+    }
+};
+const buttons = document.querySelectorAll('.single-answer');
+Array.from(buttons).forEach(function (buttons) {
+  buttons.addEventListener('click', updatecounters, false);
+});
